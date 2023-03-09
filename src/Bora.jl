b668e7597331f7d7cedbe053c4377703e16d6e61
Marco Bonici <bonici.marco@gmail.com>
2023-03-09 04:47:05 +0100

    Succesfull refactor

--- src/Bora.jl
+++ src/Bora.jl
@@ -2,33 +2,11 @@ module Bora
 
 
 using Base: @kwdef
-using LoopVectorization
-using SimpleChains
+using AbstractEmulator
 
-function maximin_input!(x, in_MinMax)
-    for i in eachindex(x)
-        x[i] -= in_MinMax[i,1]
-        x[i] /= (in_MinMax[i,2]-in_MinMax[i,1])
-    end
-end
+abstract type AbstractξℓEmulator end
 
-function inv_maximin_output!(x, out_MinMax)
-    for i in eachindex(x)
-        x[i] *= (out_MinMax[i,2]-out_MinMax[i,1])
-        x[i] += out_MinMax[i,1]
-    end
-end
-
-abstract type AbstractTrainedEmulators end
-
-@kwdef mutable struct SimpleChainsEmulator <: AbstractTrainedEmulators
-    Architecture
-    Weights
-end
-
-abstract type AbstractPℓEmulator end
-
-@kwdef mutable struct PℓEmulator <: AbstractPℓEmulator
+@kwdef mutable struct ξℓEmulator <: AbstractξℓEmulator
     TrainedEmulator::AbstractTrainedEmulators
     rgrid::Array
     InMinMax::Matrix{Float64} = zeros(7,2)
@@ -39,32 +17,28 @@ abstract type AbstractCompleteEmulator end
 
 @kwdef mutable struct CompleteEmulator <: AbstractCompleteEmulator
     rgrid::Array
-    PℓMono::AbstractPℓEmulator
-    PℓQuadru::AbstractPℓEmulator
-    PℓHexadeca::AbstractPℓEmulator
+    PℓMono::AbstractξℓEmulator
+    PℓQuadru::AbstractξℓEmulator
+    PℓHexadeca::AbstractξℓEmulator
 end
 
-function ComputePℓs(input_params, Pℓs_emu::CompleteEmulator)
-    output_l0 = ComputePℓ(input_params, Pℓs_emu.PℓMono)
-    output_l2 = ComputePℓ(input_params, Pℓs_emu.PℓQuadru)
-    output_l4 = ComputePℓ(input_params, Pℓs_emu.PℓHexadeca)
+function get_ξℓs(input_params, Pℓs_emu::CompleteEmulator)
+    output_l0 = get_ξℓ(input_params, Pℓs_emu.PℓMono)
+    output_l2 = get_ξℓ(input_params, Pℓs_emu.PℓQuadru)
+    output_l4 = get_ξℓ(input_params, Pℓs_emu.PℓHexadeca)
     return Array(hcat(output_l0, output_l2, output_l4)')
 end
 
 
-function ComputePℓ(input_params, Pℓ_emu::PℓEmulator)
+function get_ξℓ(input_params, Pℓ_emu::ξℓEmulator)
     input = deepcopy(input_params)
     maximin_input!(input, Pℓ_emu.InMinMax)
     output = Array(run_emulator(input, Pℓ_emu.TrainedEmulator))
     inv_maximin_output!(output, Pℓ_emu.OutMinMax)
-    return output#reshape(output, Int(length(output)/length(Pℓ_emu.rgrid)), :)
+    return output
 end
 
-function run_emulator(input, trained_emulator::SimpleChainsEmulator)
-    return trained_emulator.Architecture(input, trained_emulator.Weights)
-end
-
-function ComputeBroadBand(r, bbpar::Array{T}) where T
+function get_broadband(r, bbpar::Vector{T}) where T
     ℓs = [0,2,4]
     BB = zeros(T, length(ℓs),length(r))
     bbpar_reshaped = reshape(bbpar, 3,3)'
@@ -78,25 +52,29 @@ function ComputeBroadBand(r, bbpar::Array{T}) where T
     return BB
 end
 
-function ComputeBora(input_params, Pℓs_emu::CompleteEmulator)
-    cosmo_params = input_params[1:7]
-    bb_params = input_params[8:16]
-    Pls = ComputePℓs(cosmo_params, Pℓs_emu)
-    BB = ComputeBroadBand(Pℓs_emu.rgrid, bb_params)
-    return Pls .+ BB
+function get_broadband(r, bbpar::Matrix{T}) where T
+    ℓs = [0,2,4]
+    dim_pars, dim_vect = size(bbpar)
+    BB = zeros(T, length(ℓs),length(r), dim_vect)
+    bbpar_reshaped = zeros(3,3,dim_vect)
+    for i in 1:dim_vect
+        bbpar_reshaped[:,:,i] = reshape(bbpar[:,i], 3,3)'
+    end
+    norm=0.0015#the value of xi at r=rref
+    rref=80.
+    for l in 1:3
+        for i in 1:3
+            for v in 1:dim_vect
+                BB[l,:,v] .+= bbpar_reshaped[l,i,v]*r.^(-i+1)*norm*rref^(i-1)
+            end
+        end
+    end
+    return BB
 end
 
-function ComputeBora(input_cosmo, input_broadband, Pℓs_emu::CompleteEmulator)
-    Pls = ComputePℓs(input_cosmo, Pℓs_emu)
-    BB = ComputeBroadBand(Pℓs_emu.rgrid, input_broadband)
-    return Pls .+ BB
-end
-
-function ComputeBoraReduced(input_params, Pℓs_emu::CompleteEmulator)
-    cosmo_params = input_params[1:6]
-    bb_params = input_params[7:15]
-    Pls = ComputePℓs(cosmo_params, Pℓs_emu)
-    BB = ComputeBroadBand(Pℓs_emu.rgrid, bb_params)
+function get_ξℓs(cosmo_params, bb_params, Pℓs_emu::CompleteEmulator)
+    Pls = get_ξℓs(cosmo_params, Pℓs_emu)
+    BB = get_broadband(Pℓs_emu.rgrid, bb_params)
     return Pls .+ BB
 end
 
