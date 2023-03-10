114e022ce067b1363232e47eca980235d2f95fce
Marco Bonici <bonici.marco@gmail.com>
2023-03-10 02:57:25 +0100

    Fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,19 +17,18 @@ weights = SimpleChains.init_params(mlpd)
 emu = Bora.SimpleChainsEmulator(Architecture = mlpd, Weights = weights)
 r_test = Array(LinRange(0,200, 40))
 bora_emu = Bora.ξℓEmulator(TrainedEmulator = emu, rgrid=r_test, InMinMax = rand(6,2),
-                                OutMinMax = rand(6,2))
+                                OutMinMax = rand(40,2))
 
 bora_complete_emu = Bora.CompleteEmulator(rgrid=r_test, ξℓMono=bora_emu, ξℓQuad=bora_emu,
                                           ξℓHexa=bora_emu)
 
 @testset "Bora tests" begin
-    cosmo = rand(6)
-    cosmo_vec = hcat(cosmo, cosmo)
-    bb = rand(9)
-    bb_vec = hcat(bb, bb)
+    cosmo = ones(6)
+    cosmo_vec = ones(6,6)
+    bb = ones(9)
+    bb_vec = ones(9,6)
     output = Bora.get_ξℓs(cosmo, bb, bora_complete_emu)
-    output_vec = Bora.get_ξℓs(cosmo_vec, bb_vec, bora_complete_emu)
     tests_zeros = Bora.get_broadband(r_test, zeros(9,100))
     @test any(tests_zeros .== 0)
-    @test any(output .== output_vec[1,:,:])
+    output_vec = Bora.get_ξℓs(cosmo_vec, bora_complete_emu)
 end
